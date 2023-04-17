8111a57f86a25a948473e75bee47d50d945d9680
Азаревич Эдуард Александрович <azarevich_ea@peleng.jsc.local>
2023-04-17 16:31:16 +0300

    add error and update form charactor

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,4 +1,7 @@
 import { Component } from 'react';
+import Spinner from '../spinner/spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from '../../services/MarvelService';
@@ -6,46 +9,57 @@ import MarvelService from '../../services/MarvelService';
 class RandomChar extends Component {
     constructor(props) {
         super(props)
-        this.updateChar();
     }
     state = {
-        charactor: {}
+        charactor: {},
+        loading: true,
+        error: false
     }
 
     marvelService = new MarvelService();
 
-    onChatLoaded = (charactor) => {
-        this.setState({ charactor })
+    onCharLoaded = (charactor) => {
+        this.setState({ charactor, loading: false })
     }
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
             .getCharacter(id)
-            .then(this.onChatLoaded)
+            .then(this.onCharLoaded)
+            .catch(this.onError)
+    }
+    componentDidMount() {
+        this.updateChar();
+    }
+
+    componentWillUnmount() {
+        clearImmediate(this.timeId);
+    }
+
+
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    onClickTryIt = () => {
+        this.updateChar();
     }
 
     render() {
-        const { charactor: { name, description, thumbnail, homepage, wiki } } = this.state;
+        const { charactor, loading, error } = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error) ? <View characrot={charactor} /> : null;
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img" />
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {description}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br />
@@ -54,7 +68,7 @@ class RandomChar extends Component {
                     <p className="randomchar__title">
                         Or choose another one
                     </p>
-                    <button className="button button__main">
+                    <button onClick={this.onClickTryIt} className="button button__main">
                         <div className="inner">try it</div>
                     </button>
                     <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -62,6 +76,29 @@ class RandomChar extends Component {
             </div>
         )
     }
+}
+
+const View = ({ characrot }) => {
+    const { name, description, thumbnail, homepage, wiki } = characrot;
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img" />
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {description}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default RandomChar;
