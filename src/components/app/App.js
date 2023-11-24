7b34918184d29d44001a5057f40e4a20420cc969
Азаревич Эдуард Александрович <azarevich_ea@peleng.jsc.local>
2023-11-24 13:32:01 +0300

    add 404Page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { MainPage, ComicsPage } from '../pages';
+import { MainPage, ComicsPage, Page404 } from '../pages';
 import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
@@ -12,6 +12,7 @@ const App = () => {
                     <Routes>
                         <Route path="/" element={<MainPage />} />
                         <Route path="/comics" element={<ComicsPage />} />
+                        {<Route path='*' element={<Page404 />} />}
                     </Routes>
                 </main>
             </div>
