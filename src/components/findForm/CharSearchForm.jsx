08acca921f27dcf0f1c42079aa18c2a2e0ce6417
Edward <edward.azarevich@gmail.com>
2025-03-21 15:16:38 +0300

    add error with focus

--- src/components/findForm/CharSearchForm.jsx
+++ src/components/findForm/CharSearchForm.jsx
@@ -5,20 +5,25 @@ import './charSearchForm.scss';
 
 const CharSearchForm = () => {
 
-    const { register, handleSubmit, watch, formState: { errors, isValid } } = useForm();
+    const { register, handleSubmit, onBlur, watch, formState: { errors, isValid } } = useForm();
+
+    const [isInputFocused, setIsInputFocused] = React.useState(false);
 
     const onSubmit = (data) => console.log(data);
     return (
         <form className='char__search-form' onSubmit={handleSubmit(onSubmit)}>
             <label className="char__search-label" htmlFor="charName">Or find a character by name:</label>
             <div className="char__search-wrapper">
-                <input {...register("name", { required: true })} />
+                <input
+                    {...register("name", { required: true })}
+                    onFocus={() => setIsInputFocused(true)}
+                    onBlur={() => setIsInputFocused(false)}
+                />
                 <button type='submit' className="button button__main" >
                     <div className="inner">find</div>
                 </button>
             </div>
-            {errors.name && <div className='char__search-error'>This field is required</div>}
-
+            {(errors.name && isInputFocused) && <div className='char__search-error'>This field is required</div>}
         </form>
     );
 };
