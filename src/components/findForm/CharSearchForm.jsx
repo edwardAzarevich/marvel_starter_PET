04857ec63e8120fbba9f736da39ca83185fb1a44
Edward <edward.azarevich@gmail.com>
2025-03-21 15:05:50 +0300

    add reach hook form and validation

--- src/components/findForm/CharSearchForm.jsx
+++ src/components/findForm/CharSearchForm.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 import './charSearchForm.scss';
 
 const CharSearchForm = () => {
+
+    const { register, handleSubmit, watch, formState: { errors, isValid } } = useForm();
+
+    const onSubmit = (data) => console.log(data);
     return (
-        <div className='char__search-form'>
+        <form className='char__search-form' onSubmit={handleSubmit(onSubmit)}>
             <label className="char__search-label" htmlFor="charName">Or find a character by name:</label>
             <div className="char__search-wrapper">
-                <input />
+                <input {...register("name", { required: true })} />
                 <button type='submit' className="button button__main" >
                     <div className="inner">find</div>
                 </button>
             </div>
-        </div>
+            {errors.name && <div className='char__search-error'>This field is required</div>}
+
+        </form>
     );
 };
 
