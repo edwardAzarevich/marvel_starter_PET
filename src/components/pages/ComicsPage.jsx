177564d8108b5372de9a34b0361ae55729db0760
Edward <edward.azarevich@gmail.com>
2025-04-08 17:38:32 +0300

    add Helmet for main and comics page component

--- src/components/pages/ComicsPage.jsx
+++ src/components/pages/ComicsPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
+import { Helmet } from 'react-helmet';
 
 const ComicsPage = () => {
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with list of our comics"
+                />
+                <title>Comics Page</title>
+            </Helmet>
             <AppBanner />
             <ComicsList />
         </>
