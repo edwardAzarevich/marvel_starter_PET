36612f8ca4abb0430c5c2b39b767b13fe5008f5e
Edward <edward.azarevich@gmail.com>
2026-03-31 09:52:05 +0300

    fix key for all Charactor

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -26,7 +26,7 @@ const CharList = (props) => {
 
     const [charList, setCharList] = useState([]);
     const [newItemLoading, setNewItemLoading] = useState(false);
-    const [offset, setOffset] = useState(210);
+    const [offset, setOffset] = useState(8);
     const [charEnded, setCharEnded] = useState(false);
 
     const { loading, error, getAllCharacters, process, setProcess } = useMarvelService();
